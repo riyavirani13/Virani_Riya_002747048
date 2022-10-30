f965e26a4e36917aa979f9d119c0df75bac6f7ae
RIYA <virani.r@northeastern.edu>
2022-10-30 17:38:46 -0400

    commit

--- HospitalManagementSystem/src/ui/MainJFrame.java
+++ HospitalManagementSystem/src/ui/MainJFrame.java
@@ -363,7 +363,6 @@ public class MainJFrame extends javax.swing.JFrame {
         txtCommAdminCommName = new javax.swing.JTextField();
         jLabel103 = new javax.swing.JLabel();
         btnAddHospital = new javax.swing.JButton();
-        jPanel2 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(181, 212, 243));
@@ -2114,21 +2113,6 @@ public class MainJFrame extends javax.swing.JFrame {
 
         card.add(CommAdminMainJPanel, "card4");
 
-        jPanel2.setBackground(new java.awt.Color(181, 212, 243));
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1780, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1246, Short.MAX_VALUE)
-        );
-
-        card.add(jPanel2, "card2");
-
         mainSplitFrame.setRightComponent(card);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -3024,7 +3008,6 @@ public class MainJFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel92;
     private javax.swing.JLabel jLabel93;
     private javax.swing.JLabel jLabel94;
-    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
